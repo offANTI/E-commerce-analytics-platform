221e016f2e7a109dc2d7168895253b1ad41d2860
offANTI <ruslan.tulei@gmail.com>
2026-08-04 18:01:06 +0200

    Add change schema silver_dummy_product

--- azure_databricks/transform/silver_dummy_products.py
+++ azure_databricks/transform/silver_dummy_products.py
@@ -4,14 +4,14 @@ from pyspark.sql import functions as F
 from storage.silver_writer import write_to_silver
 
 
-def build_silver_dummy_products(spark: SparkSession, bronze_base: str, silver_base: str) -> None:
-    schema = "title STRING, brand STRING, category STRING, price DOUBLE, stock INT"
+def build_silver_dummy_products(spark, bronze_base: str, silver_base: str) -> None:
+    schema = "id INT, title STRING, brand STRING, category STRING, price DOUBLE, stock INT"
 
     df = (
         spark.read.format("delta").load(f"{bronze_base}/dummy_products/")
         .withColumn("parsed", F.from_json("raw_data", schema))
         .select(
-            F.col("id"),
+            F.col("parsed.id").alias("id"),
             F.col("parsed.title").alias("product_title"),
             F.col("parsed.brand").alias("brand"),
             F.col("parsed.category").alias("category"),
